576c0e0b363d926046634cb20a0e77bc3a766825
SebastianCarrero <sebastiancarrero1996@hotmail.com>
2024-12-05 19:48:32 -0500

    se corrige url para modelo

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 from PIL import Image
+import gdown
+import os
 
-# Cargar el modelo guardado
-model = load_model('C:/SEBASTIAN/BOOTCAMP IA/PROYECTO_FINAL_RED_NEURONAL_CNN/notebooks/best_model.keras')
+# Ruta local donde se guardará el modelo descargado
+MODEL_PATH = 'best_model.keras'
+
+# Descargar el modelo desde Google Drive
+if not os.path.exists(MODEL_PATH):
+    file_id = '1GUmE7-jt437-oub8QhChCGM0G3exwtRD'  # ID del archivo de Google Drive
+    url = f'https://drive.google.com/uc?id={file_id}'
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+# Cargar el modelo
+model = load_model(MODEL_PATH)
 
 # Diccionario de clases
 class_dict = {0: 'Orgánico', 1: 'Reciclable'}
@@ -21,14 +32,15 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Imagen cargada", use_column_width=True)
     st.write("Clasificando...")
-    
+
     # Preprocesar la imagen
-    image = image.resize((224, 224))
-    img_array = img_to_array(image) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    image = image.resize((224, 224))  # Ajusta al tamaño esperado por tu modelo
+    img_array = img_to_array(image) / 255.0  # Normaliza la imagen
+    img_array = np.expand_dims(img_array, axis=0)  # Expande dimensiones para hacerla compatible con el modelo
     
     # Predicción
     prediction = model.predict(img_array)
-    predicted_class = class_dict[int(prediction[0][0] > 0.5)]
-    
+    predicted_class = class_dict[int(prediction[0][0] > 0.5)]  # Predicción binaria
+
     st.write(f"La imagen pertenece a la categoría: **{predicted_class}**")
+
