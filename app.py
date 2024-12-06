a4c129a6268b557f3b0dfb2bcd3347ced67a64a5
SebastianCarrero <sebastiancarrero1996@hotmail.com>
2024-12-05 20:52:40 -0500

    se corrige url para modelo

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
+import requests
+import os
 import numpy as np
 from PIL import Image
-import gdown
-import os
 
-import zipfile
 
-# Ruta donde se guardará el modelo descomprimido
-MODEL_PATH = 'best_model.keras'
-MODEL_ZIP_PATH = 'best_model.zip'
+# Configurar la URL de Dropbox
+url = "https://www.dropbox.com/scl/fi/g1b2zjf0o8wlq9z4vgbbs/best_model.keras?dl=1"
+MODEL_PATH = "best_model.keras"
 
-# Descargar el archivo si no existe
-if not os.path.exists(MODEL_ZIP_PATH):
-    file_id = '1GUmE7-jt437-oub8QhChCGM0G3exwtRD'  # ID del archivo de Google Drive
-    url = f'https://drive.google.com/uc?id={file_id}'  # Enlace de descarga directa
-    gdown.download(url, MODEL_ZIP_PATH, quiet=False)
 
-# Si el archivo está comprimido, descomprimirlo
-if MODEL_ZIP_PATH.endswith('.zip'):
-    with zipfile.ZipFile(MODEL_ZIP_PATH, 'r') as zip_ref:
-        zip_ref.extractall('.')  # Extrae el archivo en el directorio actual
+# Función para descargar el modelo desde Dropbox si aún no existe
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        st.write("Descargando el modelo desde Dropbox...")
+        response = requests.get(url)
 
-# Verifica si el archivo existe después de la descompresión
-if os.path.exists(MODEL_PATH):
-    print(f"El archivo {MODEL_PATH} está disponible para cargar.")
-else:
-    print(f"Error: El archivo {MODEL_PATH} no se pudo descomprimir correctamente.")
+        if response.status_code == 200:
+            with open(MODEL_PATH, "wb") as file:
+                file.write(response.content)
+            st.write("Modelo descargado correctamente.")
+        else:
+            st.error(
+                f"Error al descargar el modelo desde Dropbox. Código de estado: {response.status_code}"
+            )
+            raise FileNotFoundError(
+                f"Error al descargar el modelo desde Dropbox. Código de estado: {response.status_code}"
+            )
+
+
+# Descargar el modelo si no existe
+download_model()
 
 # Cargar el modelo
+st.write("Cargando el modelo...")
 model = load_model(MODEL_PATH)
+st.write("Modelo cargado exitosamente.")
+
 
 # Diccionario de clases
-class_dict = {0: 'Orgánico', 1: 'Reciclable'}
+class_dict = {0: "Orgánico", 1: "Reciclable"}
 
-# Configuración de la interfaz
+# Configurar la interfaz
 st.title("Clasificador de Imágenes: Residuos Orgánicos y Reciclables")
 st.write("Sube una imagen para clasificarla.")
 
@@ -48,13 +56,13 @@ if uploaded_file is not None:
     st.write("Clasificando...")
 
     # Preprocesar la imagen
-    image = image.resize((224, 224))  # Ajusta al tamaño esperado por tu modelo
-    img_array = img_to_array(image) / 255.0  # Normaliza la imagen
-    img_array = np.expand_dims(img_array, axis=0)  # Expande dimensiones para hacerla compatible con el modelo
-    
-    # Predicción
+    image = image.resize((224, 224))
+    img_array = img_to_array(image) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Realizar la predicción
     prediction = model.predict(img_array)
-    predicted_class = class_dict[int(prediction[0][0] > 0.5)]  # Predicción binaria
+    predicted_class = class_dict[int(prediction[0][0] > 0.5)]
 
+    # Mostrar los resultados
     st.write(f"La imagen pertenece a la categoría: **{predicted_class}**")
-
